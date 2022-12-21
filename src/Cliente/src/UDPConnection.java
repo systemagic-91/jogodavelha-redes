2f3d45b69120cb3597ce9c7054d799a2702c9c61
Letícia Americano Lucas <leticia.americano.lucas@gmail.com>
2022-12-21 05:30:11 -0300

    Fixed udp

--- src/Cliente/src/UDPConnection.java
+++ src/Cliente/src/UDPConnection.java
@@ -8,11 +8,6 @@ class UDPConnection
    private static int portaServidor = 9988;
    private static byte[] sendData = new byte[1024];
    private static byte[] receiveData = new byte[1024];
-
-   public static byte[] lerString () throws Exception {
-      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-      return in.readLine().getBytes();
-   }
    
    public String receivePackge;
 
@@ -20,7 +15,7 @@ class UDPConnection
    {
       DatagramSocket clientSocket = new DatagramSocket();
       InetAddress ipServidor = InetAddress.getByName("::1");
-      sendData = lerString();
+      sendData = "board";
 
       DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, ipServidor, portaServidor);
       clientSocket.send(sendPacket);
